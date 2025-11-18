6c601bec6c4438ef4c048ccfa823385b1291ff3c
julia-dancona <juliadancona8@gmail.com>
2025-11-18 01:52:29 -0300

    subindo repp

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function mostrarTrimestre(numero) {
-    const secoes = document.querySelectorAll('section');
-    secoes.forEach(secao => {
-      if (secao.id === `trimestre${numero}`) {
-        secao.classList.add('ativo');
-      } else {
-        secao.classList.remove('ativo');
-      }
-    });
-  }
+  const secoes = document.querySelectorAll('section');
+  secoes.forEach(secao => {
+    if (secao.id === `trimestre${numero}`) {
+      secao.classList.add('ativo');
+    } else {
+      secao.classList.remove('ativo');
+    }
+  });
+}
 
-mostrarTrimestre('trimestre1'); 
+mostrarTrimestre('trimestre1');
